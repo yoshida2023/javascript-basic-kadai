cdf78807c711c4e713aa0a1afa975ce02c448ffc
yoshida2025 <127272004+yoshida2023@users.noreply.github.com>
2025-01-23 21:12:56 +0900

    mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,1 +1,1 @@
-console.log(new Date().getFullYear() + '年' + new Date().getMonth() + '月' + new Date().getDate() + '日');
+console.log(new Date().getFullYear() + '年' + new Date().getMonth()+1 + '月' + new Date().getDate() + '日');
