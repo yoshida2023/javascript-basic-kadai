70d5cf7f4c22624d29924189ce56d399aa0e8027
yoshida2025 <127272004+yoshida2023@users.noreply.github.com>
2025-05-12 21:59:07 +0900

    Add files via upload

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,7 +1,7 @@
-$('.box').on('load', function(){
+$(window).on('load', function(){
     console.log('loadイベントが発生しました');
 });
 
-$('.box').on('scroll', function(){
+$(window).on('scroll', function(){
     console.log('scrollイベントが発生しました');
 });
